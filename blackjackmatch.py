824e299de73840f22b6cc29d0730c7ced9827f77
SangMinLee-97 <dltkd79@gmail.com>
2021-03-04 17:18:01 +0900

    feat: Define blackjackmatch()

---
+++ blackjackmatch.py
@@ -0,0 +1,8 @@
+def match(player1, player2):
+    if player1 > player2:
+        print('player1 Win!')
+    elif player1 < player2:
+        print('player2 Win!')
+    elif player1 = player2:
+        print('draw')
+
